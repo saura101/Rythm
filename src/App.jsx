1862750671540f02ec5dcc08cdf7d91644391eb8
Souradeep <souradeepghosh00@gmail.com>
2024-01-08 15:10:58 +0530

    added components

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Playlist from './components/Playlist'
+import Sidebar from './components/Sidebar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
       </div>
-      <h1>Vite</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
+      <h1>Hello world</h1>
+      <Header/>
+      <Footer/>
+      <Playlist/>
+      <Sidebar/>
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <div>
+            <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2018/11/Spotify_Logo_RGB_Green.png"/>
+        </div>
+    );
+}
+
+export default Header;
